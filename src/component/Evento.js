ba8a8b69f32b9a01e6ebc0271619d2f9a4274337
Christian Toscano <chrismarcadopago@gmail.com>
2019-07-03 17:36:19 -0300

    extraerdescripcio  mostrarlo en el componente, validacion para cortar descripcion

--- src/component/Evento.js
+++ src/component/Evento.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
+
 const Evento = ({evento}) => {
+
+
+        //extraer el texto
+        let { text } = evento.description;
+        //cortar la descripción 
+        if( text) {
+            if(text.length > 250) {
+                text = text.substr(0 , 250);
+            }
+        }else {
+            text = null;
+        }
     return ( 
         <div>
             <div className="uk-card uk-card-default">
@@ -12,10 +25,11 @@ const Evento = ({evento}) => {
                 
                 <div className="uk-card-body">
                     <h3 className="uk-card-title">{evento.name.text}</h3>
+                    <p>{ text }</p>
                 </div>
 
                 <div className="uk-card-footer">
-                    <a href={evento.url} className="uk-button uk-button-secondary">
+                    <a href={evento.url} className="uk-button uk-button-secondary" target="_blank" rel="noopener noreferrer">
                         Más Información
                     </a>
                 </div>
